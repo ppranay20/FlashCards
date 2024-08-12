7cafbf8f0f17f320118bdce61de3dba63cd38d13
ppranay20 <pranay20.parakh@gmail.com>
2024-08-12 21:42:07 +0530

    completed

---
+++ client/src/seeddata.js
@@ -0,0 +1,16 @@
+const data = [
+    {
+        question : "Time complexity of linear search?",
+        answer : "O(n)"
+    },
+    {
+        question : "What is the capital of INDIA?",
+        answer : "Delhi"
+    },
+    {
+        question : "Who is the Prime Minister of India?",
+        answer : "Narendra Modi"
+    }
+]
+
+export default data;
